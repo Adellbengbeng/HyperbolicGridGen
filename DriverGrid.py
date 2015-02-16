7bda9449a02de2a30e7564fd087fbf411e25dd34
adegenna <adegenna@princeton.edu>
2015-02-15 22:54:05 -0500

    Added grid methods to spline the airfoil coordinates

--- DriverGrid.py
+++ DriverGrid.py
@@ -7,7 +7,11 @@ import grid
 
 gridfile = "NACA23012.dat"
 mygrid = grid.Grid(gridfile)
-plt.plot(mygrid.X,mygrid.Y,'-o')
+mygrid.interpolateSurf()
+
+X = mygrid.X
+Y = mygrid.Y
+plt.plot(X,Y,'-o')
 plt.axis('equal')
 plt.draw()
 plt.show()
--- grid.py
+++ grid.py
@@ -1,7 +1,9 @@
 import numpy as np
 import random
 import os
+from scipy.interpolate import interp1d
 
+# Class to handle a 2D hyperbolic grid
 class Grid:
     def __init__(self,gridfile):
         # Initialization
@@ -12,8 +14,30 @@ class Grid:
         fullpath = airfoildirectory + gridfile
         with open(fullpath, "r") as file:
             XY = [[float(digit) for digit in line.split()] for line in file]
-        self.X = np.zeros([len(XY),1])
-        self.Y = np.zeros([len(XY),1])
-        for i in range(0,len(self.X)):
-            self.X[i] = XY[i][0]
-            self.Y[i] = XY[i][1]
+        X = np.zeros([len(XY),1])
+        Y = np.zeros([len(XY),1])
+        for i in range(0,len(X)):
+            X[i] = XY[i][0]
+            Y[i] = XY[i][1]
+        self.X = np.concatenate(X); self.Y = np.concatenate(Y);
+    def calcScoords(self):
+        # Function to calculate s-coordinates of grid (x,y) coordinates
+        X = self.X; Y = self.Y;
+        DX = np.diff(X); DY = np.diff(Y);
+        DX = np.insert(DX,0,0); DY = np.insert(DY,0,0);
+        DS = np.sqrt(np.power(DX,2) + np.power(DY,2))
+        self.S = np.cumsum(DS)
+    def interpolateSurf(self):
+        # Function to interpolate the grid (x,y) coordinates
+        X = self.X; Y = self.Y;
+        self.calcScoords()
+        s = self.S
+        fX = interp1d(s,X,kind='cubic')
+        fY = interp1d(s,Y,kind='cubic')
+        t2 = np.linspace(0,s[len(s)-1],400)
+        self.X = fX(t2); self.Y = fY(t2);
+    #def calcDXDxi(self):
+        # Function to differentiate grid (x,y) coordinates in the xi direction
+        # Second-order, central differencing
+        
+        
